fdcb61f150be9f690fc203e71b23fb3caf5e3753
Cleiton Dsd <s.diascleiton@gmail.com>
2019-01-22 18:07:07 -0200

    Código em PHP para verificar existência do RM no BD

---
+++ php/buscaRm.php
@@ -0,0 +1,32 @@
+<!-- /* PHP *//* Cleiton Dsd - www.github.com/CleitonDsd - dev.cleitondsd@gmail.com - @cleitonDsd (twitter)*/
+/*Algumas funções serão comentadas, pois ainda não consigo manipular todas'*/
+
+/*<>Código para Pegar o Rm do Aluno Trazer o nome Form de Empréstimos<>'*/
+/*<>Code to get Rm<>'*/
+ -->
+<?php
+
+include_once './conecta.php';
+
+$rmAluno = filter_input(INPUT_GET, 'rmAluno', FILTER_SANITIZE_STRING);
+if(!empty($rmAluno)){
+    
+    $limit = 1;
+    $result_aluno = "SELECT * FROM alunos WHERE rmAluno =:rmAluno LIMIT :limit";
+    
+    $resultado_aluno = $conn->prepare($result_aluno);
+    $resultado_aluno->bindParam(':rmAluno', $rmAluno, PDO::PARAM_STR);
+    $resultado_aluno->bindParam(':limit', $limit, PDO::PARAM_INT);
+    $resultado_aluno->execute();
+    
+    $array_valores = array();
+    
+    if($resultado_aluno->rowCount() != 0){
+        $row_aluno = $resultado_aluno->fetch(PDO::FETCH_ASSOC);
+        $array_valores['nomeAluno'] = $row_aluno['nomeAluno'];         
+    }else{                
+        $array_valores['nomeAluno'] = 'Aluno não encontrado'; 
+
+    }
+    echo json_encode($array_valores);
+}
