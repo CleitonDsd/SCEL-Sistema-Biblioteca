bf06db7fad3de05324dd858aca5e9af157d4dfe7
Cleiton Dsd <s.diascleiton@gmail.com>
2019-01-23 18:26:59 -0200

    Comentando arquivo que pega os valores dos input e envia para queryEditora - getEditora

--- js/script_getEditora.js
+++ js/script_getEditora.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
+	// <>pega o valor do campo com nome 'nomeLivro'. E quando clicar fora vai acessar a função<>'
     $("input[name='nomeLivro']").blur(function () {
-
-        var $editoraLivro  = $("input[name='editoraLivro']");        
+    	// <> variáveis que vão receber os dados do campo<>'
+        var $editoraLivro  = $("input[name='editoraLivro']"); 
+        // recupera o nome do Livro do input e coloca na váriavel       
         var nomeLivro     = $(this).val();
-        
+         // <>Chama o arquivo responsável em buscar no Banco de Dados, e envia o Livro pesquisado<>'
         $.getJSON('queryEditora.php', {nomeLivro},
+        	// <>Function vai recuperar o que esse arquivo retorna e colocar na variável 'retorno'<>'
             function(retorno){
+            	// <>A váriavel $nomeEditora vai receber o que vier de retorno da posição nomeEditora(input)<>'
                 $editoraLivro.val(retorno.editoraLivro);             
             }
         );        
