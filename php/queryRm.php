69c1a2ad766fcc397c31ea50f2942e3af023de9a
Cleiton Dsd <s.diascleiton@gmail.com>
2019-01-23 16:48:26 -0200

    Código para buscar nome do Aluno através da consulta do Rm no BD

---
+++ php/queryRm.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once './conecta.php';
+
+$rmAluno = filter_input(INPUT_GET, 'rmAluno', FILTER_SANITIZE_STRING);
+if(!empty($rmAluno)){
+    
+    $limit = 1;
+    $result_aluno = "SELECT * FROM alunos WHERE rmAluno =:rmAluno LIMIT :limit";
+    
+    $resultado_aluno = $conn->prepare($result_aluno);
+    $resultado_aluno->bindParam(':rmAluno', $rmAluno, PDO::PARAM_STR);
+    $resultado_aluno->bindParam(':limit', $limit, PDO::PARAM_INT);
+    $resultado_aluno->execute();
+    
+    $array_valores = array();
+    
+    if($resultado_aluno->rowCount() != 0){
+        $row_aluno = $resultado_aluno->fetch(PDO::FETCH_ASSOC);
+        $array_valores['nomeAluno'] = $row_aluno['nomeAluno'];         
+    }else{
+        $array_valores['nomeAluno'] = 'Aluno não encontrado!';        
+    }
+    echo json_encode($array_valores);
+}
+
+/* PHP *//* Cleiton Dsd - www.github.com/CleitonDsd - dev.cleitondsd@gmail.com - @cleitonDsd (twitter)*/
+
+/*<>Código para buscar Rm e trazer Nome do Aluno<>'*/
+/*<>Code to get Rm <>'*/
