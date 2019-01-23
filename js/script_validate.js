9bb905e4582594143badce9a2f571b218f578997
Cleiton Dsd <s.diascleiton@gmail.com>
2019-01-23 16:52:42 -0200

    Inserindo ID do Form de Cadastro de Empréstimo nas validações, e alterando requerimentos

--- js/script_validate.js
+++ js/script_validate.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 					nomeAluno: { 
 						required: true,
 						maxlength: 150,
-						minlength: 10,
+						minlength: 8,
 						minWords: 2
 					},
 					emailAluno: {
@@ -97,27 +97,69 @@ $(document).ready(function(){
 $(document).ready(function(){	
 	$("#formCadastroUsuarios").validate({
 		rules:{
-					nomeUsuario: { 
+			nomeUsuario: { 
+				required: true,
+				maxlength: 150,
+				minlength: 10,
+				minWords: 2
+			},
+			emailUsuario: {
+				required: true,
+				email: true,
+				maxlength: 250,
+				minlength: 10
+			},					
+			loginUsuario: {
+				required: true,
+			},	
+			funcaoUsuario: {
+				required: true,
+			},				
+			senhaUsuario: {
+				required: true,
+				minlength: 4
+			}					
+		},												
+	})
+})
+// <>Validação do Formulário de Empréstimos<>'
+$(document).ready(function(){	
+	$("#formCadEmp").validate({
+		rules:{
+					// nome do campo do Formulário {
+					// 	se é requerido, obrigatório (required),
+					// 	máximo de caracteres (maxlength),
+					// 	minímo de caracteres (minlength),
+					// 	minímo de frases (minWords)
+					// }
+					dataEmprestimo: { 
+						required: true,						
+					},
+					dataDevolucao: { 
+						required: true,						
+					},
+					nomeLivro: {
 						required: true,
 						maxlength: 150,
-						minlength: 10,
+						minlength: 4						
+					},
+					editoraLivro: {
+						required: true,
+						maxlength: 250,
+						minlength: 3						
+					},
+					rmAluno: {
+						required: true,
+						min: 3
+					},
+					nomeAluno: {
+						required: true,
+						maxlength: 150,
+						minlength: 8,
 						minWords: 2
 					},
-					emailUsuario: {
-						required: true,
-						email: true,
-						maxlength: 250,
-						minlength: 10
-					},					
-					loginUsuario: {
-						required: true,
-					},	
-					funcaoUsuario: {
-						required: true,
-					},				
-					senhaUsuario: {
-						required: true,
-						minlength: 4
+					quantidadeRetirada: {
+						required: true,						
 					}					
 				},												
 			})
