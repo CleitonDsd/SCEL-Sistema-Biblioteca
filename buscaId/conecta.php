0887d8d45b9314e8e36c28e7b63c5ba4af2e9770
Cleiton Dsd <s.diascleiton@gmail.com>
2019-01-28 19:39:26 -0200

    Arquivo para teste de edição de dados

--- buscaId/conecta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Credenciais de acesso ao BD
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '37844460');
-define('DBNAME', 'biblioteca');
-
-$conn = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME . ';', USER, PASS);
