1fb2c9c314ee904d7198bed70134af5b3554c3cd
Cleiton Dsd <s.diascleiton@gmail.com>
2019-02-20 17:28:46 -0300

    Busca pelo Rm do aluno e preenchimento automático dos campos relacionado ao aluno no form de cadastro de empréstimo

--- js/script_getRm.js
+++ js/script_getRm.js
@@ -1,16 +1,18 @@
 
 $(document).ready(function () {
-	// <>pega o valor do campo com nome RM aluno. E quando clicar fora vai acessar a função<>'
+	// <>pega o valor do campo com nome Id aluno. E quando clicar fora vai acessar a função<>'
     $("input[name='rmAluno']").blur(function () {
     	// <> variáveis que vão receber os dados do campo<>'
+        var $idAluno  = $("input[name='idAluno']");        
         var $nomeAluno  = $("input[name='nomeAluno']");        
-        // recupera o RM do input e coloca na váriavel
+        // recupera o ID do input e coloca na váriavel
         var rmAluno     = $(this).val();
-        // <>Chama o arquivo responsável em buscar no Banco de Dados, e envia o RM pesquisado<>'
+        // <>Chama o arquivo responsável em buscar no Banco de Dados, e envia o Id pesquisado<>'
         $.getJSON('queryRm.php', {rmAluno},
         	// <>Function vai recuperar o que esse arquivo retorna e colocar na variável 'retorno'<>'
             function(retorno){
             	// <>A váriavel $nomeAluno vai receber o que vier de retorno da posição nomeAluno(input)<>'
+                $idAluno.val(retorno.idAluno);  
                 $nomeAluno.val(retorno.nomeAluno);             
             }
         );        
