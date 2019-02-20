a4cfe6ef48c69ae679087179c4dd05ef1b984ccf
Cleiton Dsd <s.diascleiton@gmail.com>
2019-02-20 17:28:13 -0300

    Busca pelo Nome do Livro e preenchimento automático dos campos relacionado ao livro no form de cadastro de empréstimo

--- js/script_getEditora.js
+++ js/script_getEditora.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
 	// <>pega o valor do campo com nome 'nomeLivro'. E quando clicar fora vai acessar a função<>'
     $("input[name='nomeLivro']").blur(function () {
     	// <> variáveis que vão receber os dados do campo<>'
-        var $editoraLivro  = $("input[name='editoraLivro']"); 
+        var $idlivro      = $("input[name='idlivro']"); 
+        var $edicaoLivro  = $("input[name='edicaoLivro']"); 
+        var $editoraLivro = $("input[name='editoraLivro']"); 
         // recupera o nome do Livro do input e coloca na váriavel       
         var nomeLivro     = $(this).val();
          // <>Chama o arquivo responsável em buscar no Banco de Dados, e envia o Livro pesquisado<>'
@@ -10,6 +12,8 @@ $(document).ready(function () {
         	// <>Function vai recuperar o que esse arquivo retorna e colocar na variável 'retorno'<>'
             function(retorno){
             	// <>A váriavel $nomeEditora vai receber o que vier de retorno da posição nomeEditora(input)<>'
+                $idlivro.val(retorno.idlivro);
+                $edicaoLivro.val(retorno.edicaoLivro);
                 $editoraLivro.val(retorno.editoraLivro);             
             }
         );        
